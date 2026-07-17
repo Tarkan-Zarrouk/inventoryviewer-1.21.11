4e9ef4c123a62679e8e0d2e97c069345b66b105b
Tarkan-Zarrouk <tarkan.zarrouk@gmail.com>
2026-07-17 17:08:22 -0400

    feat!: inventory renderer

--- src/main/java/inventory/viewer/Inventoryviewer.java
+++ src/main/java/inventory/viewer/Inventoryviewer.java
@@ -1,30 +1,34 @@
 package inventory.viewer;
 
+import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.ModInitializer;
-
+import net.fabricmc.fabric.api.client.rendering.v1.hud.HudElementRegistry;
+import net.minecraft.client.Minecraft;
 import net.minecraft.resources.Identifier;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class Inventoryviewer implements ModInitializer {
-	public static final String MOD_ID = "inventoryviewer";
+import inventory.viewer.hud.InventoryHUDElement;
 
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
+public class InventoryViewer implements ModInitializer, ClientModInitializer {
+	public static final String MOD_ID = "inventoryviewer";
+	public static Minecraft mc = Minecraft.getInstance();
+	private static final Identifier HUD_ELEMENT_IDENTIFIER = id(MOD_ID);
+
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
 		LOGGER.info("Hello Fabric world!");
 	}
 
 	public static Identifier id(String path) {
 		return Identifier.fromNamespaceAndPath(MOD_ID, path);
+	}
+
+	@Override
+	public void onInitializeClient() {
+		HudElementRegistry.addLast(HUD_ELEMENT_IDENTIFIER, new InventoryHUDElement());
 	}
 }
---
+++ src/main/java/inventory/viewer/hud/InventoryHUDElement.java
@@ -0,0 +1,33 @@
+package inventory.viewer.hud;
+
+import inventory.viewer.InventoryViewer;
+import net.fabricmc.fabric.api.client.rendering.v1.hud.HudElement;
+import net.minecraft.client.DeltaTracker;
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.player.LocalPlayer;
+import net.minecraft.client.renderer.RenderPipelines;
+import net.minecraft.resources.Identifier;
+import net.minecraft.world.entity.player.Inventory;
+import net.minecraft.world.item.ItemStack;
+
+public class InventoryHUDElement implements HudElement {
+    @Override
+    public void render(GuiGraphics graphics, DeltaTracker tickCounter) {
+        LocalPlayer mcPlayer = InventoryViewer.mc.player;
+        Inventory playerInventory = mcPlayer.getInventory();
+        Identifier inventoryBackgroundIdentifier = Identifier.fromNamespaceAndPath(InventoryViewer.MOD_ID, "textures/gui/inventory.png");
+        graphics.blit(RenderPipelines.GUI_TEXTURED, inventoryBackgroundIdentifier, 90, 90, 0, 0, 167, 59, 167, 59); //graphics.blit(constDefByGameEng, x,y, posSkipU, posSkipV, borderWidth, borderHeight, textureWidth, textureHeight);
+        for(int i = 9; i < Inventory.INVENTORY_SIZE;i++) {
+            ItemStack itemObj = playerInventory.getItem(i);
+            int index = i - 9;
+            int col = index % 9;
+            int row = index / 9;
+            int x = 94 + (col * 18);
+            int y = 94 + (row * 18);
+            // System.out.println("X: " + x + ", Y: " + y);
+            graphics.renderItem(itemObj, x, y);
+            graphics.renderItemDecorations(InventoryViewer.mc.font, itemObj, x, y);
+
+        }
+    }
+}
